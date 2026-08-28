240171e028096e8741ade41e00562ff4d8f3c5f4
Planeflyer0111 <skibiditoiletslayer1@outlook.com>
2026-08-28 03:50:27 +0000

    Problem 2 complete

--- problem-2.js
+++ problem-2.js
@@ -1,12 +1,51 @@
 import { readText, writeText } from "./common.js";
-
 console.log("\n" + "Code for problem two starts" + "\n");
 main2();
-
 function main2() {
-  const voteText = readText("vote-10.txt");
-
-  console.log(voteText);
-
-  writeText(voteText);
+  const voteText = readText("vote-1000.txt")
+    .split("\n")
+    .filter(n => n);
+  let firstNames = [];
+  let lastNames = [];
+  let votes = [];
+  let extravotes = [];
+  let thing = "";
+  let y = 0;
+  let x = 0;
+  let i = 0;
+  voteText.forEach(name => {
+    const parts = name.trim().replace(/'/g, "").split(" ");
+    firstNames.push(parts[0]);
+    lastNames.push(parts[1]);
+  });
+  console.log(firstNames);
+  for (let k = 0; k < firstNames.length; k++) {
+    if (firstNames[k] == null) {
+      continue;
+    }
+    thing = firstNames[k];
+    x = 0;
+    for (i = 0; i < firstNames.length; i++) {
+      if (thing == firstNames[i]) {
+        x++;
+        firstNames[i] = null;
+      }
+    }
+    votes[y] = x;
+    extravotes[y] = lastNames[k];
+    console.log(thing + " voted for " + x + " times");
+    y++;
+  }
+  console.log("Votes = " + votes);
+  let winner = 0;
+  for (let k = 1; k < votes.length; k++) {
+    if (votes[k] > votes[winner]) {
+      winner = k;
+    }
+    else if (votes[k] == votes[winner]) {
+      if (extravotes[k] > extravotes[winner]) {
+        winner = k;
+      }
+    }
+  } writeText("vote-1000-winner.txt", extravotes[winner]);
 }

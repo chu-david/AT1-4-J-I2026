4d2a3d5af2f5407618e0e4d22c54de063d21ac36
AlphaSurgeMask <265499763+AlphaSurgeMask@users.noreply.github.com>
2026-08-28 03:51:21 +0000

    Mostly done with problem 1

--- problem-1.js
+++ problem-1.js
@@ -4,16 +4,33 @@ console.log("Code for problem one starts" + "\n");
 main1();
 
 function main1() {
+  let currentGroup = 1;
   let anagramText = readText("anagram1.txt")
     .split("\n")
     .filter((n) => n);
-  for (let i = 0; i < anagramText.length; i++) {
-    let group = [anagramText[i]];
-    for (let j = i + 1; j < anagramText.length; j++) {
+  while (anagramText.length != 0) {
+    let group = [anagramText[0]];
+    for (let j = 1; j < anagramText.length; j++) {
       if (group[0].length == anagramText[j].length) {
         group.push(anagramText[j]);
       }
     }
-    writeText("anagram" + i + ".txt", group);
+    for (let j = 0; j < group.length; j++) {
+      let counter = 0;
+      for (let h = 0; h < group[0].length; h++) {
+        if (group[0][h] == group[j][h]) {
+          counter++;
+        }
+      }
+      if (counter != group[0].length) {
+        group.splice(group.indexOf(group[j]), 1);
+      }
+    }
+    for (let j = 0; j < group.length; j++) {
+      anagramText.splice(anagramText.indexOf(group[j]), 1);
+    }
+    console.log("\n");
+    // writeText("anagram" + currentGroup + ".txt", group);
+    currentGroup++;
   }
 }
